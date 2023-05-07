4722e165176cbdb571ed9e011faf514785c750d3
황현태 <47472389+Oneul-hyeon@users.noreply.github.com>
2023-05-08 00:51:13 +0900

    [Gold V] Title: 상어 초등학교, Time: 252 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/21608. 상어 초등학교/상어 초등학교.py
+++ 백준/Gold/21608. 상어 초등학교/상어 초등학교.py
@@ -1,48 +1,71 @@
+# -*- coding: utf-8 -*-
 import sys
-input = sys.stdin.readline
+# 해당 지점의 가중치 계산
+def get_weight(classroom, student, row, col, n):
+    global like_students
 
-n = int(input())
-student = dict()
-order = []
-# 1. 딕셔너리에 학생의 번호 별 좋아하는 학생의 정보 입력
-# 2. 리스트에 학생의 자리 배치 순서 입력
-for _ in range(n**2) :
-    line = list(map(int, input().split()))
-    student[line[0]] = line[1:]
-    order.append(line[0])
-# 3. 배열 생성 <- 학생의 자리
-graph = [[0 for _ in range(n)] for _ in range(n)]
-# 4. 이중 for문을 동해 학생이 어디에 앉을지 선정 후 앉히기
-dirs = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-for who in order :
-    where = []
-    for x in range(n) :
-        for y in range(n) :
-            if graph[x][y] != 0 : continue
-            like, empty = 0, 0
-            for dir in dirs :
-                nx, ny = x + dir[0], y + dir[1]
-                if nx < 0 or nx >= n or ny < 0 or ny >= n : continue
-                # 주변에 좋아하는 학생이 얼마나 있는지
-                if graph[nx][ny] in student[who] : like += 1
-                # 주변에 빈 자리가 얼마나 있는지
-                elif graph[nx][ny] == 0 : empty += 1
-            where.append((like, empty, x, y))
-    # 조건에 만족하는 칸에 학생 앉히기
-    where.sort(key = lambda x : [-x[0], -x[1], x[2], x[3]])
-    graph[where[0][2]][where[0][3]] = who
+    weight = .0
+    for r, c in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
+        # classroom 범위 이내인지 확인
+        if (check_row := row + r) < 0 or (check_row >= n) or \
+                (check_col := col + c) < 0 or (check_col >= n):
+            continue
+        # 인접한 자리에 학생이 있는 경우, 좋아하는 학생인지 확인
+        if (adjoining_seat := classroom[check_row][check_col]) in like_students[student]:
+            weight += 1
+        # 인접한 자리가 비어있는지 확인
+        if not adjoining_seat:
+            weight += .1
+        weight = round(weight, 1)
+    return weight
 
-# 5. 이중 for문을 통해 만족도의 합 계산
-satisfaction = 0
-how = [0, 1, 10, 100, 1000]
-for x in range(n) :
-    for y in range(n) :
-        count = 0
-        for dir in dirs :
-            nx, ny = x + dir[0], y + dir[1]
-            if nx < 0 or nx >= n or ny < 0 or ny >= n : continue
-            if graph[nx][ny] in student[graph[x][y]] : count += 1
-        satisfaction += how[count]
 
-# 6. 결과 출력
-print(satisfaction)
+# 가중치를 이용하여 student가 들어갈 위치 탐색
+def get_coord(classroom, student, n):
+    MAX_WEIGHT, ROW, COL = 0, 1, 2
+
+    save = {MAX_WEIGHT: -1, ROW: -1, COL: -1}
+    for row in range(n):
+        for col in range(n):
+            # 해당 위치에 학생 배치가 된 경우
+            if classroom[row][col]:
+                continue
+            # 가중치가 저장되어 있는 값보다 적은 경우
+            if (weight := get_weight(classroom, student, row, col, n)) <= save[MAX_WEIGHT]:
+                continue
+            save[MAX_WEIGHT], save[ROW], save[COL] = weight, row, col
+    return (save[ROW], save[COL])
+
+
+like_students = dict()
+
+
+def solution():
+    global like_students
+
+    n = int(sys.stdin.readline())
+    classroom = [[0 for _ in range(n)] for _ in range(n)]
+
+    # student dictionary 제작
+    for _ in range(n * n):
+        student, *likes = map(int, sys.stdin.readline().split())
+        like_students[student] = likes
+
+    # classroom에 student 배치
+    for student in like_students.keys():
+        row, col = get_coord(classroom, student, n)
+        classroom[row][col] = student
+
+    points = {0: 0, 1: 1, 2: 10, 3: 100, 4: 1000}
+
+    # 최종 만족도 점수 계산
+    point = 0
+    for row in range(n):
+        for col in range(n):
+            weight = get_weight(classroom, classroom[row][col], row, col, n)
+            point += points[weight]
+    print(point)
+
+
+if __name__ == "__main__":
+    solution()
