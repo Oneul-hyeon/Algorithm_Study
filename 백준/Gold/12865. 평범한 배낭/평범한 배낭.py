7532eb0dd2b396c23dd835d9a93cf33fe774750d
황현태 <47472389+Oneul-hyeon@users.noreply.github.com>
2023-05-25 22:22:56 +0900

    [Gold V] Title: 평범한 배낭, Time: 4684 ms, Memory: 35108 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -2,16 +2,17 @@ import sys
 input = sys.stdin.readline
 
 n, k = map(int, input().split())
-array = [[0, 0]] + [list(map(int, input().split())) for _ in range(n)]
-# 1. 2차원 배열 생성하기
-dp = [0 for _ in range(k+1)]
-
-# 2.
-for i in range(n+1) :
+# 1. 배낭에 넣을 수 있는 물건들 입력받기
+bag = [list(map(int, input().split())) for _ in range(n)]
+# 2. 1차원 dp 생성
+dp = [0] * (k+1)
+# 3.
+for i in range(n) :
     for j in range(k, -1, -1) :
-        w = array[i][0]
-        v = array[i][1]
+        w = bag[i][0]
+        v = bag[i][1]
         if j >= w :
+            # 4. 점화식에 따라 dp 처리
             dp[j] = max(dp[j], dp[j-w] + v)
-# 3. 결과 출력
-print(dp[k])
+# 5. 결과 출력
+print(dp[-1])
