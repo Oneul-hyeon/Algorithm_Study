47b9047fa1e1dabcbc343f0da482f38cb8a5a232
황현태 <47472389+Oneul-hyeon@users.noreply.github.com>
2023-06-13 00:15:22 +0900

    [Silver I] Title: 신입 사원, Time: 5876 ms, Memory: 71876 KB -BaekjoonHub

--- 백준/Silver/1946. 신입 사원/신입 사원.py
+++ 백준/Silver/1946. 신입 사원/신입 사원.py
@@ -4,12 +4,17 @@ input = sys.stdin.readline
 t = int(input())
 for _ in range(t) :
     n = int(input())
-    # 1. 신입사원의 성적 정렬하기
+    # 1. 순위 정렬
     score = sorted([list(map(int, input().split())) for _ in range(n)])
+    # 2. 비교 인덱스 순위 설정
+    now = score[0]
     ans = 1
-    now_x, now_y = score[0][0], score[0][1]
-    for x, y in score[1:] :
-        if now_x < x and now_y < y : continue
-        now_x, now_y = x, y
+    # 3.
+    for i in range(1, n) :
+        # 4. 비교 인덱스보다 순위가 낮을 경우 뽑힐 수 없으므로 continue
+        if now[1] < score[i][1] : continue
+        # 5. 이외의 경우 뽑히므로 카운트 후 비교 인덱스 갱신
         ans += 1
+        now = score[i]
+    # 6. 결과 출력
     print(ans)
