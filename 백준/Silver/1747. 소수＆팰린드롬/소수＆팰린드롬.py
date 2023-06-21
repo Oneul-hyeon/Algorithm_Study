93dc902d96f73e9f44fe09bfcfd9f44b86c4e65f
황현태 <47472389+Oneul-hyeon@users.noreply.github.com>
2023-06-21 22:18:04 +0900

    [Silver I] Title: 소수&팰린드롬, Time: 3428 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1747. 소수＆팰린드롬/소수＆팰린드롬.py
+++ 백준/Silver/1747. 소수＆팰린드롬/소수＆팰린드롬.py
@@ -1,20 +1,21 @@
 import sys
 input = sys.stdin.readline
 
+# 4. check 재귀 함수 선언
 def check(num) :
-    # 1. 소수 여부 판별
+    # 4-1. 소수 여부 판별
     for i in range(2, int(num**0.5)+1) :
         if num % i == 0 : return False
-    # 2. 팰린드롬 여부 판별
+    # 4-2. 팰린드롬 여부 판별
     return True if str(num) == str(num)[::-1] else False
 
 n = int(input())
+# 1. n=1일 경우 처리
 if n == 1 : n += 1
+# 2.
 while True :
+    # 3. check 재귀함수 실행
     if check(n) :
         print(n)
         break
     n += 1
-
-
-
