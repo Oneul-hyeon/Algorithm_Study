c50eb9c4167ae09fd41b59007e95c70db9adb7b3
황현태 <47472389+Oneul-hyeon@users.noreply.github.com>
2023-06-30 01:04:47 +0900

    [Gold V] Title: LCS, Time: 588 ms, Memory: 55712 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.py
+++ 백준/Gold/9251. LCS/LCS.py
@@ -3,14 +3,16 @@ input = sys.stdin.readline
 
 string1, string2 = input().rstrip(), input().rstrip()
 l1, l2 = len(string1)+1, len(string2)+1
-dp = [[0] * (l1) for _ in range(l2)]
+# LCS dp 생성
+LCS = [[0] * l1 for _ in range(l2)]
 
 for i in range(1, l2) :
     for j in range(1, l1) :
         # 문자가 같을 경우
         if string2[i-1] == string1[j-1] :
-            dp[i][j] = dp[i-1][j-1] + 1
+            LCS[i][j] = LCS[i-1][j-1] + 1
+        # 문자가 같지 않을 경우
         else :
-            dp[i][j] = max(dp[i][j-1], dp[i-1][j])
+            LCS[i][j] = max(LCS[i][j-1], LCS[i-1][j])
 
-print(dp[-1][-1])
+print(LCS[-1][-1])
