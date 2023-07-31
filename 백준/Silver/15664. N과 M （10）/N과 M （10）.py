6527f3a7261769e26828cfd069094943b4775595
황현태 <47472389+Oneul-hyeon@users.noreply.github.com>
2023-07-31 12:03:18 +0900

    [Silver II] Title: N과 M (10), Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/15664. N과 M （10）/N과 M （10）.py
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.py
@@ -10,7 +10,7 @@ def permutation(lst, length, idx) :
         return
     # 1-2.
     for i in range(idx, n) :
-        # 해당 인덱스 값 리스트에 추가
+        # 수열 함수 실행
         permutation(lst + [array[i]], length + 1, i+1)
 
 n, m = map(int, input().split())
