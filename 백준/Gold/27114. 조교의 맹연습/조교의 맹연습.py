1c385e396a80e205fd270925f4e86530ec8e8566
황현태 <47472389+Oneul-hyeon@users.noreply.github.com>
2023-08-06 17:20:08 +0900

    [Gold IV] Title: 조교의 맹연습, Time: 612 ms, Memory: 227464 KB -BaekjoonHub

--- 백준/Gold/27114. 조교의 맹연습/조교의 맹연습.py
+++ 백준/Gold/27114. 조교의 맹연습/조교의 맹연습.py
@@ -3,7 +3,7 @@ input = sys.stdin.readline
 
 a, b, c, k = map(int, input().split())
 # 1. 냅색 리스트 생성
-dp = [[sys.maxsize] * (4) for _ in range(k+1)]
+dp = [[1000001] * 4 for _ in range(k+1)]
 # 2. 초기값 설정
 dp[0][0] = 0
 
@@ -13,7 +13,9 @@ for i in range(k+1) :
         # 3-1. 예외처리
         if i + cost > k : continue
         for j in range(4) :
+            if dp[i][j] == 1000001 : continue
             # 3-2.
-            dp[i+cost][(j+dir)%4] = min(dp[i][j] + 1, dp[i+cost][(j+dir)%4])
+            direction = (j+dir) % 4
+            dp[i+cost][direction] = min(dp[i][j] + 1, dp[i+cost][direction])
 
-print(-1) if dp[k][0] == sys.maxsize else print(dp[k][0]) 
+print(-1) if dp[k][0] == 1000001 else print(dp[k][0])
