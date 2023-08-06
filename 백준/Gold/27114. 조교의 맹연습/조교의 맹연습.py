10c8dddd660cca39fc9089f604c02c70f59de1ce
황현태 <47472389+Oneul-hyeon@users.noreply.github.com>
2023-08-06 17:27:04 +0900

    [Gold IV] Title: 조교의 맹연습, Time: 416 ms, Memory: 146548 KB -BaekjoonHub

--- 백준/Gold/27114. 조교의 맹연습/조교의 맹연습.py
+++ 백준/Gold/27114. 조교의 맹연습/조교의 맹연습.py
@@ -3,19 +3,19 @@ input = sys.stdin.readline
 
 a, b, c, k = map(int, input().split())
 # 1. 냅색 리스트 생성
-dp = [[1000001] * 4 for _ in range(k+1)]
+dp = [[1000001] * (k+1) for _ in range(4)]
 # 2. 초기값 설정
 dp[0][0] = 0
 
 # 3.
-for i in range(k+1) :
+for j in range(k+1) :
     for cost, dir in [(a,1), (b, 3), (c, 2)] :
         # 3-1. 예외처리
-        if i + cost > k : continue
-        for j in range(4) :
+        if j + cost > k : continue
+        for i in range(4) :
             if dp[i][j] == 1000001 : continue
             # 3-2.
-            direction = (j+dir) % 4
-            dp[i+cost][direction] = min(dp[i][j] + 1, dp[i+cost][direction])
+            direction = (i+dir) % 4
+            dp[direction][j+cost] = min(dp[i][j] + 1, dp[direction][j+cost])
 
-print(-1) if dp[k][0] == 1000001 else print(dp[k][0])
+print(-1) if dp[0][k] == 1000001 else print(dp[0][k])
