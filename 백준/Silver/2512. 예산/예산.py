c6892779f1b7b3d4fe96c61c3ed861780740f6cc
황현태 <47472389+Oneul-hyeon@users.noreply.github.com>
2023-08-08 18:41:58 +0900

    [Silver II] Title: 예산, Time: 76 ms, Memory: 34400 KB -BaekjoonHub

---
+++ 백준/Silver/2512. 예산/예산.py
@@ -0,0 +1,25 @@
+import sys, math
+input = sys.stdin.readline
+
+n = int(input())
+array = list(map(int, input().split()))
+m = int(input())
+
+# 1. left, right 설정
+left, right = m // n, max(array)
+# 2.
+while left <= right :
+    # 2-1. mid 값 구하기
+    mid = (left+right) // 2
+    # 2-2. 배정되는 예산 구하기
+    cost = 0
+    for c in array :
+        cost += mid if mid <= c else c
+    # 2-3. 배정되는 예산이 총 예산보다 작을 경우
+    if cost < m: left = mid + 1
+    # 2-3. 배정되는 예산이 총 예산보다 클 경우
+    elif cost > m: right = mid - 1
+    # 2-4. 배정되는 예산이 총 예산과 같을 경우
+    else : right = mid; break
+# 3. 결과 출력
+print(right)
