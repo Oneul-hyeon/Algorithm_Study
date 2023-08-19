115653e24297d4a39be66f22e47f3b1dbdf3cbe6
황현태 <47472389+Oneul-hyeon@users.noreply.github.com>
2023-08-19 16:09:32 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 64 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -1,27 +1,30 @@
-import sys, math
+import sys
+input = sys.stdin.readline
 
-n = int(sys.stdin.readline())
-numbers = list(map(int, sys.stdin.readline().split()))
-plus, sub, mul, div = map(int, sys.stdin.readline().split())
-
-def dfs(id, sum, plus, sub, mul, div):
-    global min_, max_
-    if id == n :
-        min_ = min(min_, sum)
-        max_ = max(max_, sum)
+# 1. 백트래킹 함수 정의
+def backtracking(num, idx, plus, sub, mul, div) :
+    global max_ans, min_ans
+    # 1-1. 종료조건 설정
+    if idx == n :
+        # 최댓값 업데이트
+        max_ans = max(max_ans, num)
+        # 최솟값 업데이트
+        min_ans = min(min_ans, num)
         return
-    if plus :
-        dfs(id+1, sum+numbers[id], plus-1, sub, mul, div)
-    if sub :
-        dfs(id+1,sum-numbers[id],plus, sub-1, mul, div)
-    if mul :
-        dfs(id+1,sum*numbers[id],plus, sub, mul-1, div)
-    if div :
-        if sum < 0 : dfs(id+1,-(-sum//numbers[id]), plus, sub, mul, div-1)
-        else : dfs(id+1,sum//numbers[id], plus, sub, mul, div-1)
+    # 1-2. 더하기 개수가 남은 경우
+    if plus : backtracking(num + numbers[idx], idx + 1, plus - 1, sub, mul, div)
+    # 1-3. 빼기 개수가 남은 경우
+    if sub : backtracking(num - numbers[idx], idx + 1, plus, sub - 1, mul, div)
+    # 1-4. 곱하기 개수가 남은 경우
+    if mul : backtracking(num * numbers[idx], idx + 1, plus, sub, mul - 1, div)
+    # 1-5. 나누기 개수가 남은 경우
+    if div : backtracking(num // numbers[idx], idx + 1, plus, sub, mul, div - 1) if num > 0 else backtracking(-(abs(num) // numbers[idx]), idx + 1, plus, sub, mul, div - 1)
 
-min_, max_ = math.inf, -math.inf
-id = 1
-dfs(id, numbers[0], plus, sub, mul, div)
-print(max_)
-print(min_)
+n = int(input())
+numbers = list(map(int, input().split()))
+plus, sub, mul, div = map(int, input().split())
+max_ans, min_ans = -sys.maxsize, sys.maxsize
+# 2. 백트래킹 함수 실행
+backtracking(numbers[0], 1, plus, sub, mul, div)
+# 3. 결과 출력
+print(f'{max_ans}\n{min_ans}')
