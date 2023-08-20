3f46b835f31062d9ad791b53250fed932691127f
황현태 <47472389+Oneul-hyeon@users.noreply.github.com>
2023-08-21 00:24:06 +0900

    [Gold IV] Title: 스도쿠, Time: 2036 ms, Memory: 134768 KB -BaekjoonHub

--- 백준/Gold/2580. 스도쿠/스도쿠.py
+++ 백준/Gold/2580. 스도쿠/스도쿠.py
@@ -16,17 +16,17 @@ def check_squre(x, y, num) :
         for j in range(y//3 * 3, y//3 * 3 + 3) :
             if sudoku[i][j] == num : return False
     return True
-# 5. 백트래킹 함수 정의
+# 4. 백트래킹 함수 정의
 def backtracking(n) :
-    # 5-1. 종료 조건 설정
+    # 4-1. 종료 조건 설정
     if n == len(blank) :
-        # 모든 빈 칸을 다 채운 경우이므로 스토쿠 출력 후 시스템 종료
+        # 모든 빈 칸을 다 채운 경우이므로 스도쿠 출력 후 시스템 종료
         for line in sudoku :
             print(*line)
         exit()
-    # 5-2. 빈 칸의 인덱스 가져오기
+    # 4-2. 빈 칸의 인덱스 가져오기
     x, y = blank[n]
-    # 5-3.
+    # 4-3.
     for num in range(1, 10) :
         # 현재 인덱스에 해당 수 설정이 가능할 경우
         if check_row(x, num) and check_col(y, num) and check_squre(x, y, num) :
@@ -38,10 +38,10 @@ def backtracking(n) :
             sudoku[x][y] = 0
 
 sudoku = [list(map(int, input().split())) for _ in range(9)]
-# 6. 빈 칸 인덱스 리스트 생성
+# 5. 빈 칸 인덱스 리스트 생성
 blank = []
 for i in range(9) :
     for j in range(9) :
         if sudoku[i][j] == 0 : blank.append((i, j))
-# 7. 백트래킹 함수 실행
+# 6. 백트래킹 함수 실행
 backtracking(0)
