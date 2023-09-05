e494c14ff15b40ea9dff7b36636ea749de0aadcf
황현태 <47472389+Oneul-hyeon@users.noreply.github.com>
2023-09-05 22:36:48 +0900

    [Silver I] Title: 주지수, Time: 1000 ms, Memory: 72532 KB -BaekjoonHub

---
+++ 백준/Silver/15724. 주지수/주지수.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+graph = [[0 for _ in range(m+1)]] + [[0] + list(map(int, input().split())) for _ in range(n)]
+# 1. 누적합 배열 생성
+prefix_sum = graph.copy()
+# 2. 누적합 생성
+# 2-1. 열 별
+for j in range(1, m+1) :
+    for i in range(1, n+1) :
+        prefix_sum[i][j] += prefix_sum[i-1][j]
+# 2-2. 행 별
+for i in range(1, n+1) :
+    for j in range(1, m+1) :
+        prefix_sum[i][j] += prefix_sum[i][j-1]
+
+t = int(input())
+for _ in range(t) :
+    x1, y1, x2, y2 = map(int, input().split())
+    # 3. 점화식에 따른 사람 수의 합 출력
+    print(prefix_sum[x2][y2] - prefix_sum[x2][y1-1] - prefix_sum[x1-1][y2] + graph[x1-1][y1-1])
