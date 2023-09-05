dda4448e502a94321e575c916d8419657a5a2454
황현태 <47472389+Oneul-hyeon@users.noreply.github.com>
2023-09-05 23:35:35 +0900

    [Silver I] Title: 통나무 건너뛰기, Time: 220 ms, Memory: 32276 KB -BaekjoonHub

--- 백준/Silver/11497. 통나무 건너뛰기/통나무 건너뛰기.py
+++ 백준/Silver/11497. 통나무 건너뛰기/통나무 건너뛰기.py
@@ -6,7 +6,7 @@ for _ in range(t) :
     n = int(input())
     array = sorted(list(map(int, input().split())))
     # 1. 한 개 차이 중 최솟값 계산
-    answer = min( array[1] - array[0], array[-1] - array[-2])
+    answer = max( array[1] - array[0], array[-1] - array[-2] )
     # 2. 두 개 차이 중 최댓값 계산
     for i in range(2, n) :
         answer = max(answer, array[i] - array[i-2])
