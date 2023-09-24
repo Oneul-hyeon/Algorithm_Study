475041885b0429c778e4c2afee08a5164f88b90f
황현태 <47472389+Oneul-hyeon@users.noreply.github.com>
2023-09-24 17:32:27 +0900

    [level 3] Title: 이중우선순위큐, Time: 0.05 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/lv3/42628. 이중우선순위큐/이중우선순위큐.py
+++ 프로그래머스/lv3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -1,26 +1,50 @@
-from heapq import heappush, heappop, heapify, nlargest
+from heapq import heappush, heappop
+from collections import defaultdict
+
 
 def solution(operations):
-    # 1. 힙 리스트 생성
-    heap = []
-    # 2.
+    # 1. 최소힙, 최대힙 선언
+    min_heap, max_heap = [], []
+    # 2. 최대힙 딕셔너리, 최소힙 딕셔너리 선언
+    max_dict, min_dict = defaultdict(int), defaultdict(int)
+    # 3.
     for command in operations :
-        # 2-1. 삽입 명령의 경우
-        if command[0] == 'I' :
-            # 최대, 최소 힙에 삽입
-            num = int(command.split(' ')[1])
-            heappush(heap, num)
-        # 2-2. 삭제 명령일 경우
+        # 3-1. 삽입 명령일 경우
+        c1, c2 = command.split(' ')
+        if c1 == 'I' :
+            num = int(c2)
+            # 최대, 최소힙에 삽입
+            heappush(max_heap, -num)
+            heappush(min_heap, num)
+            # 각 딕셔너리에 카운트
+            max_dict[num] += 1
+            min_dict[num] += 1
+        # 3-2. 삭제 명령일 경우
         else :
             # 힙이 비었을 경우 무시
-            if not heap: continue
+            if not max_heap or not min_heap :
+                if max_heap or min_heap :
+                    min_heap, max_heap = [], []
+                    max_dict, min_dict = defaultdict(int), defaultdict(int)
+                continue
             # 최솟값 삭제 명령일 경우
-            if command.split(' ')[1] == '-1':
-                print(heap)
-                heappop(heap)
+            if c2 == '-1' :
+                while min_heap :
+                    num = heappop(min_heap)
+                    # 최소 힙 딕셔너리에 해당 최솟값 1 감소
+                    if min_dict[num] > 0 : min_dict[num] -= 1
+                    # 최소 힙에서 추출한 최솟값이 최대힙에 있는 경우
+                    if min_dict[num] < max_dict[num] : break
             # 최댓값 삭제 명령일 경우
-            else:
-                heap = nlargest(len(heap), heap)[1:]
-                heapify(heap)
-    # 3. 결과 출력
-    return [0, 0] if not heap else [nlargest(1, heap)[0], heap[0]]
+            else :
+                while max_heap :
+                    num = -heappop(max_heap)
+                    # 최소 힙 딕셔너리에 해당 최솟값 1 감소
+                    if max_dict[num] > 0 : max_dict[num] -= 1
+                    # 최소 힙에서 추출한 최솟값이 최대힙에 있는 경우
+                    if min_dict[num] > max_dict[num] : break
+    # 4. 최소, 최대 힙 병합
+    for i in range(len(max_heap)) : max_heap[i] *= -1
+    last_heap = sorted(set(max_heap) & set(min_heap))
+    # 5. 결과 출력
+    return [0, 0] if not last_heap else [last_heap[-1], last_heap[0]]
