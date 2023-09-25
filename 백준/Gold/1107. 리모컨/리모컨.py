1ca0c4e4e718ef493ea96f00131bab37000e64d5
황현태 <47472389+Oneul-hyeon@users.noreply.github.com>
2023-09-25 13:06:20 +0900

    [Gold V] Title: 리모컨, Time: 932 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/1107. 리모컨/리모컨.py
+++ 백준/Gold/1107. 리모컨/리모컨.py
@@ -7,7 +7,7 @@ broken = list(input().rstrip().split())
 
 # 1. +, - 만으로 갈 수 있는 최대 입력 횟수 구하기
 min_count = abs(100 - n)
-# 2. 1000000의 범위의 값들을 확인하며 최솟값 업데이트하기
+
 for i in range(500000*2 + 1) :
     ch = str(i)
     for x in ch :
