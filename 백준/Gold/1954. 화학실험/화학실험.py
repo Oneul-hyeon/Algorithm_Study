994b5847ac4235aa81a92c1a622c9b29b7dc7a55
황현태 <47472389+Oneul-hyeon@users.noreply.github.com>
2023-11-05 22:30:00 +0900

    [Gold IV] Title: 화학실험, Time: 108 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/1954. 화학실험/화학실험.py
+++ 백준/Gold/1954. 화학실험/화학실험.py
@@ -4,16 +4,14 @@ input = sys.stdin.readline
 def solution(n, array, total) :
     # 1. 조합 함수 정의
     def combination(target, now) :
-        state = True
         # 1-1.
         for a, b in array :
             # 필요한 만큼 시약 감소
             now -= (target - b) / a
-            if state and (int(now) != now) : state = False
         # 1-2. 시약의 양이 부족하다면 0 출력
         if now < 0 : print(0); exit()
         # 1-3. 정확히 목표 가스 양을 사용했다면 결과 출력 후 종료
-        if now == 0 and state : print(target); exit()
+        if now == 0 : print(target); exit()
 
     # 2. 최소 시작 값 정의
     target = 1
