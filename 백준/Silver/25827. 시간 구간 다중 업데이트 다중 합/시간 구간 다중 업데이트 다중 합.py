0c77c2856ff27b95944c88c86e0473b53ee0d69d
황현태 <47472389+Oneul-hyeon@users.noreply.github.com>
2023-11-25 19:10:27 +0900

    [Silver I] Title: 시간 구간 다중 업데이트 다중 합, Time: 468 ms, Memory: 36604 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/25827. 시간 구간 다중 업데이트 다중 합/시간 구간 다중 업데이트 다중 합.py
+++ 백준/Silver/25827. 시간 구간 다중 업데이트 다중 합/시간 구간 다중 업데이트 다중 합.py
@@ -1,37 +1,42 @@
 import sys
 input = sys.stdin.readline
-state = False
+
 def solution(n) :
-    # 1. 명령 1
-    def command(c,start, end) :
-        global state
-        # 1-1. 시, 분, 초 정보를 초 정보로 변환
-        start = sum([x*y for x, y in zip(map(int, start.split(":")), [3600, 60, 1]) if x])
-        end = sum([x*y for x, y in  zip(map(int, end.split(":")), [3600, 60, 1])if x])
-        # 1-2. 명령 1일 경우
-        if c == '1' :
-            # 누적합을 위한 값 삽입
-            for i in range(start+1, end+1) :
-                seconds[i] += 1
-        # 1-3. 명령 2일 경우
-        else :
-            # 1-3-1. 최초 누적합 수행
-            if not state : prefix_sum()
-            # 1-3-2. 값 출력
-            print(seconds[end] - seconds[start])
-    # 2. 누적합
-    def prefix_sum() :
-        global state
-        state = True
-        for i in range(2, 24 * 60 * 60 + 1) :
-            seconds[i] += seconds[i-1]
-    # 3. 초 당 배열 생성
-    seconds = [0 for _ in range(24*60*60 + 1)]
+    # 1. 누적합 수행 함수 정의
+    def run_prefix_sum() :
+        prefix_sum[0] = change[0]
+        # 1-1.
+        for i in range(1, l+1) :
+            # 점화식에 따라 처리
+            prefix_sum[i] = prefix_sum[i-1] + change[i]
+        # 1-2.
+        for i in range(1, l+1) :
+            # 누적합 계산 수행
+            prefix_sum[i] += prefix_sum[i-1]
+
+    # 2. 누적합, 변화값 리스트 생성
+    l = 24 * 60 * 60
+    prefix_sum, change = [0 for _ in range(l+1)], [0 for _ in range(l+1)]
+    # 3.누적합 최초 실행 여부 변수 생성
+    state = False
     # 4.
     for _ in range(n) :
-        c, start, end = map(str, input().rstrip().split(' '))
-        command(c, start, end)
-
+        information = list(map(str, input().rstrip().split(' ')))
+        # 4-1. 시간 구간을 초로 변환
+        start, end = sum([x * s for x, s in zip(map(int, information[1].split(':')), [3600, 60, 1]) if x]), sum([x * s for x, s in zip(map(int, information[2].split(':')), [3600, 60, 1]) if x])
+        # 4-2. 유형 1의 경우
+        if information[0] == '1' :
+            # 4-2-1. 변화값 입력
+            change[start] += 1
+            change[end] -= 1
+        # 4-3. 유형 2의 경우
+        else :
+            # 4-3-1. 최초 누적합 실행
+            if not state :
+                state = True
+                run_prefix_sum()
+            # 4-3-2. 값 출력
+            print(prefix_sum[end - 1] - prefix_sum[start - 1]) if start else print(prefix_sum[end - 1])
 if __name__ == "__main__" :
     n = int(input())
     solution(n)
