20bb3d4f7c9fed86cf48668b008aaca3982d3029
황현태 <47472389+Oneul-hyeon@users.noreply.github.com>
2024-01-02 08:37:34 +0900

    [Gold III] Title: 이세계 게임, Time: 416 ms, Memory: 35380 KB -BaekjoonHub

--- 백준/Gold/2314. 이세계 게임/이세계 게임.py
+++ 백준/Gold/2314. 이세계 게임/이세계 게임.py
@@ -19,45 +19,46 @@ def solution():
 
     # 2. bfs 함수 정의
     def bfs(start):
+        # 2-1. 비트 변환 함수 정의
         def check(bit, idx1, idx2, cnt):
+            # 2-2-1. 주어진 두 비트 변환
             bit ^= (1 << idx1)
             bit ^= (1 << idx2)
-
-            # 타켓과 같을 경우 자리 바꿈 횟수 + 1 반환
+            # 2-2-2. 타겟과 같을 경우 자리 바꿈 횟수 + 1 반환
             if bit == target:
                 print(cnt+1)
                 exit()
+            # 2-2-3. 이외의 경우
             else:
                 # 방문하지 않은 경우
                 if not visited[bit]:
                     # 방문 처리
                     visited[bit] = True
+                    # 큐에 (다음 비트, 자리 바꿈 횟수 + 1) 삽입
                     queue.append((bit, cnt + 1))
 
-        # 2-1. 큐에 (현재 배치 비트, 0) 삽입
+        # 2-2. 큐에 (현재 배치 비트, 0) 삽입
         queue = deque()
         queue.append((start, 0))
-        # 2-2. 방문 여부 딕셔너리 생성 후 방문 처리
+        # 2-3. 방문 여부 딕셔너리 생성 후 방문 처리
         visited = defaultdict(bool)
         visited[start] = True
-        # 2-3.
+        # 2-4.
         while queue:
-            # 2-3-1. 큐에서 (현재 배치 비트, 자리 바꿈 횟수) 반환
+            # 2-4-1. 큐에서 (현재 배치 비트, 자리 바꿈 횟수) 반환
             now, cnt = queue.popleft()
-            # 2-3-2.
+            # 2-4-2.
             for i in range(4):
                 for j in range(4):
-                    # 타겟과 다를 경우
                     idx = 4 * i + j
                     # 열변환
-                    # 현재 인덱스의 좌우가 다른 문자일 경우 비트 변환
                     r = idx + 1
-                    if r < 4 * (i + 1) and (now & (1 << idx) != now & (1 << r)):
-                        check(now, idx, r, cnt)
+                    # 현재 문자와 오른쪽 문자가 다른 문자일 경우 비트 변환
+                    if r < 4 * (i + 1) and (now & (1 << idx) != now & (1 << r)): check(now, idx, r, cnt)
                     # 행 변환
                     d = idx + 4
-                    if d <= 16 and (now & (1 << idx) != now & (1 << d)):
-                        check(now, idx, d, cnt)
+                    # 현재 문자와 아래쪽 문자가 다른 문자일 경우 비트 변환
+                    if d <= 16 and (now & (1 << idx) != now & (1 << d)): check(now, idx, d, cnt)
 
     now = [list(input().rstrip()) for _ in range(4)]
     target = []
