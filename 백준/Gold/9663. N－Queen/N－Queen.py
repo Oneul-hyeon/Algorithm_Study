1d7371fdbffab3141c5c4627c9c6c2dee1c9af00
황현태 <47472389+Oneul-hyeon@users.noreply.github.com>
2024-01-04 22:50:24 +0900

    [Gold IV] Title: N-Queen, Time: 29104 ms, Memory: 199080 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.py
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -1,29 +1,32 @@
-def adjacent(x): # x와 i가 같으면 행이 같은거
-    for i in range(x):
-        # 열이 같거나 대각선에 위치라면 false
-        if row[x] == row[i] or abs(row[x]-row[i]) == x-i:
-            return False
+import sys
+input = sys.stdin.readline
+
+# 1. 배치 가능 여부 함수 정의
+def check(x) :
+    # 1-1.
+    for i in range(x) :
+        # 같은 열에 있거나 대각선에 위치할 경우 False 반환
+        if queen[x] == queen[i] or abs(queen[x] - queen[i]) == x - i : return False
+    # 1-2. True 반환
     return True
+# 2. 백트래킹 함수 정의
+def dfs(x) :
+    global ans
+    # 2-1. 종료 조건 설정
+    if x == n :
+        ans += 1
+        return
+    # 2-2.
+    for i in range(n) :
+        queen[x] = i
+        # 2-2-1. 배치 가능할 경우 백트래킹 실행
+        if check(x) : dfs(x+1)
 
-def dfs(x):
-    global result
-
-    if x == N:
-        result += 1
-    else:
-        # 각 행에 퀸 놓아보기
-        for i in range(N):
-            row[x] = i
-            # 행, 열 대각선 체크 true 반환시 다음 줄 체크
-            # 백트래킹
-            if adjacent(x):
-                dfs(x+1)
-
-
-
-N = int(input())
-row = [0] * N
-result = 0
-
+ans = 0
+n = int(input())
+# 3. queen 리스트 생성
+queen = [0] * n
+# 4. 백트래킹 함수 실행
 dfs(0)
-print(result)
+# 5. 결과 출력
+print(ans)
