e8bbe47d03c2cab8a469d18bd7004109e6c142f4
HyeonTae <47472389+Oneul-hyeon@users.noreply.github.com>
2024-07-11 21:56:13 +0900

    [Gold V] Title: LCS, Time: 492 ms, Memory: 56548 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.py
+++ 백준/Gold/9251. LCS/LCS.py
@@ -1,18 +1,17 @@
 import sys
 input = sys.stdin.readline
 
-string1, string2 = input().rstrip(), input().rstrip()
-l1, l2 = len(string1)+1, len(string2)+1
-# 1. LCS dp 생성
-LCS = [[0] * l1 for _ in range(l2)]
+string1, string2 = " " + input().strip(), " " + input().strip()
+# 1. LCS 리스트 생성
+LCS = [[0 for _ in range(len(string2))] for _ in range(len(string1))]
 # 2.
-for i in range(1, l2) :
-    for j in range(1, l1) :
-        # 2-1. 문자가 같을 경우
-        if string2[i-1] == string1[j-1] :
+for i in range(1, len(string1)) :
+    for j in range(1, len(string2)) :
+        # 2-1. 비교하는 문자가 같을 경우 점화식 처리
+        if string1[i] == string2[j] :
             LCS[i][j] = LCS[i-1][j-1] + 1
-        # 2-2. 문자가 같지 않을 경우
+        # 2-2. 비교하는 문자가 다를 경우 점화식 처리
         else :
-            LCS[i][j] = max(LCS[i][j-1], LCS[i-1][j])
+            LCS[i][j] = max(LCS[i-1][j], LCS[i][j-1])
 # 3. 결과 출력
 print(LCS[-1][-1])
