0cc7679e28594b2a9e1e72a1afc35faf1d4e9ee6
HyeonTae <47472389+Oneul-hyeon@users.noreply.github.com>
2024-07-15 21:56:18 +0900

    [Silver I] Title: 문자열 교환, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1522. 문자열 교환/문자열 교환.py
@@ -0,0 +1,29 @@
+import sys
+input = sys.stdin.readline
+
+string = input().strip()
+
+# 1. 윈도우 사이즈 설정
+window_size = string.count("a")
+# 2. a가 없을 경우 0 출력
+if window_size == 0 : print(0)
+# 3. 이외의 경우
+else :
+    # 3-1. 순환 문자열 생성
+    # if window_size > 1 : string = string[-(window_size-1) :] + string
+    string += string
+    # 3-2. 문자별 카운트 딕셔너리 생성
+    count = {"a" : 0, "b" : 0}
+    # 3-3. 초기값 설정
+    for s in string[:window_size] : count[s] += 1
+    # 3-4.
+    ans = float("INF")
+    for end in range(window_size, len(string)) :
+        start = end - window_size + 1
+        # 3-4-1. 문자별 카운트 딕셔너리 업데이트
+        count[string[start-1]] -= 1
+        count[string[end]] += 1
+        # 3-4-2. 최솟값 업데이트
+        ans = min(ans, count["b"])
+    # 3-5. 결과 출력
+    print(ans)
