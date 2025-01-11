68849aa5bef8bccdfc3fcb8cb128b6357c820373
HyeonTae <47472389+Oneul-hyeon@users.noreply.github.com>
2025-01-11 14:38:05 +0900

    [level 3] Title: 정수 삼각형, Time: 51.90 ms, Memory: 14.2 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.py
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.py
@@ -1,12 +1,16 @@
 def solution(triangle):
     # 1.
-    for i in range(1, len(triangle)) :
-        for j in range(len(triangle[i])) :
-            # i행의 0번 인덱스일 경우
-            if j == 0 : triangle[i][j] += triangle[i-1][j]
-            # i행의 i번 인덱스일 경우
-            elif j == i : triangle[i][j] += triangle[i-1][j-1]
-            # 이외의 경우
-            else : triangle[i][j] += max(triangle[i-1][j-1], triangle[i-1][j])
-    # 2. 결과 리턴
+    for x, row in enumerate(triangle) :
+        if x == 0 : continue
+        for y, num in enumerate(row) :
+            # 1-1. 가장 왼쪽의 경우
+            if y == 0 :
+                triangle[x][y] += triangle[x-1][y]
+            # 1-2. 가장 오른쪽의 경우
+            elif y == len(row)-1 :
+                triangle[x][y] += triangle[x-1][y-1]
+            # 1-3. 이외의 경우
+            else :
+                triangle[x][y] += max(triangle[x-1][y], triangle[x-1][y-1])
+    # 2. 최댓값 return
     return max(triangle[-1])
