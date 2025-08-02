ef5f2c1cf5dd5bd4383c748cf7e3e2ee07533329
HyeonTae <47472389+Oneul-hyeon@users.noreply.github.com>
2025-08-02 16:59:25 +0900

    [Gold V] Title: 점프 게임, Time: 196 ms, Memory: 38948 KB -BaekjoonHub

--- 백준/Gold/15558. 점프 게임/점프 게임.py
+++ 백준/Gold/15558. 점프 게임/점프 게임.py
@@ -11,12 +11,12 @@ def checker() -> Annotated[int, "0(클리어 불가) or 1(클리어 가능)"]:
     # 1-2. 큐에 초기 위치 삽입
     queue.append((0, 0, 0))
     # 1-3. 방문 여부 리스트 생성 후 현재 위치 방문 처리
-    visited = [0, 0]
-    visited[0] |= (1<<0)
+    visited = [[False] * n for _ in range(2)]
+    visited[0][0]=True
     # 1-4.
     while queue:
         # 1-4-1. 큐에서 값 추출
-        line, position, time = queue.pop()
+        line, position, time = queue.popleft()
         # 1-4-2.
         for jump, dist in [(0, 1), (0, -1), (1, k)] :
             # 다음 줄, 위치 선정
@@ -25,10 +25,10 @@ def checker() -> Annotated[int, "0(클리어 불가) or 1(클리어 가능)"]:
             if next_position >= n:
                 return 1
             # 예외 처리
-            if next_position <= time or not board[next_line][next_position] or visited[next_line]&(1<<next_position) :
+            if next_position <= time or not board[next_line][next_position] or visited[next_line][next_position] :
                 continue
             # 현재 위치 방문 처리
-            visited[next_line] |= (1<<next_position)
+            visited[next_line][next_position]=True
             # 큐에 (다음 줄, 다음 위치, 시간) 삽입
             queue.append((next_line, next_position, time+1))
     # 게임 클리어를 못 하는 경우
