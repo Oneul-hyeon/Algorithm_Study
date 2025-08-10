506cf540652a9d6275b1d8960edc785e72bf8066
HyeonTae <47472389+Oneul-hyeon@users.noreply.github.com>
2025-08-10 10:32:19 +0900

    [Gold V] Title: 빗물, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/14719. 빗물/빗물.py
+++ 백준/Gold/14719. 빗물/빗물.py
@@ -1,17 +1,20 @@
 import sys
 input = sys.stdin.readline
 
-_, w = map(int, input().split())
-array = list(map(int, input().split()))
+h, w = map(int, input().split())
+blocks = list(map(int, input().split()))
 
-# 1. 출력 변수 생성
+# 1. 빗물의 총량 변수 생성
 ans = 0
-# 2,
-for idx, height in enumerate(array) :
-    if idx == 0 or idx == len(array) - 1 : continue
-    # 2-1. 해당 인덱스에 차오를 물의 높이 구하기
-    min_height = min(max(array[:idx]), max(array[idx+1:]))
-    # 2-2. 해당 높이가 현재 높이보다 높을 경우 출력 변수 업데이트
-    if (mod := min_height - height) > 0 : ans += mod
+# 2.
+for idx, height in enumerate(blocks):
+    # 2-1. 예외 처리
+    if idx == 0 or idx == w-1 : continue
+    # 2-2. 물이 고일 수 있는 가능 높이 선정
+    possible_height = min(max(blocks[:idx]), max(blocks[idx+1:]))
+    # 2-3. 가능 높이가 현재 열의 높이보다 높을 경우
+    if (mod:=possible_height-height) > 0 :
+        # 빗물의 총량 업데이트
+        ans += mod
 # 3. 결과 출력
 print(ans)
