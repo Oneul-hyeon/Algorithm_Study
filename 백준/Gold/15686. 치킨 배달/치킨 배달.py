d213b39104968980f30e0420a397247164a90fd4
HyeonTae <47472389+Oneul-hyeon@users.noreply.github.com>
2025-08-17 21:09:17 +0900

    [Gold V] Title: 치킨 배달, Time: 216 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.py
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.py
@@ -2,26 +2,43 @@ import sys
 from itertools import combinations
 input = sys.stdin.readline
 
+# 1. 도시의 치킨 거리 계산 함수 정의
+def calculate_chicken_distance_of_city(store) :
+    # 1-1. 각 집의 치킨 거리 계산 함수 정의
+    def calculate_chicken_distance(x, y) :
+        # 1-1-1. 최솟값 변수 정의
+        min_dist = float("INF")
+        # 1-1-2.
+        for chicken_x, chicken_y in store :
+            min_dist = min(min_dist, abs(x - chicken_x) + abs(y - chicken_y))
+        # 1-1-3. 치킨 거리 반환
+        return min_dist
+    # 1-2. 도시의 치킨 거리 변수 정의
+    chicken_distance_of_city = 0
+    # 1-3.
+    for x, y in home :
+        # 도시의 치킨 거리 업데이트
+        chicken_distance_of_city += calculate_chicken_distance(x, y)
+    # 1-4. 도시의 치킨 거리 반환
+    return chicken_distance_of_city
+
 n, m = map(int, input().split())
 graph = [list(map(int, input().split())) for _ in range(n)]
-home, chicken = [], []
 
-# 집, 치킨 집 위치 찾기
-for i in range(n) :
-    for j in range(n) :
-        if graph[i][j] == 1 :
-            home.append((i, j))
-        elif graph[i][j] == 2 :
-            chicken.append((i, j))
-
-# 살려둘 치킨집 인덱스 구하기
-combination = combinations(range(len(chicken)), m)
-
-min_ans = int(1e9)
-for com in combination :
-    ans = 0
-    for x, y in home :
-        ans += min([abs(x - chicken[i][0]) + abs(y - chicken[i][1]) for i in com])
-    min_ans = min(min_ans, ans)
-
-print(min_ans)
+# 2. 집 리스트 생성
+# 3. 치킨 집 리스트 생성
+home, store = [], []
+for x in range(n) :
+    for y in range(n) :
+        if graph[x][y] == 1 :
+            home.append((x, y))
+        elif graph[x][y] == 2 :
+            store.append((x, y))
+# 4. 출력 변수 정의
+ans = float("INF")
+# 5.
+for combination in combinations(store, m) :
+    # 5-1 도시의 치킨 거리의 최솟값 업데이트
+    ans = min(ans, calculate_chicken_distance_of_city(combination))
+# 6. 결과 출력
+print(ans)
