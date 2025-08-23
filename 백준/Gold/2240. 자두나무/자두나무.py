af7943f0f32741a7ac9a0739f29abd6f3691cf2d
HyeonTae <47472389+Oneul-hyeon@users.noreply.github.com>
2025-08-24 01:38:17 +0900

    [Gold IV] Title: 자두나무, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/2240. 자두나무/자두나무.py
+++ 백준/Gold/2240. 자두나무/자두나무.py
@@ -1,26 +1,21 @@
 import sys
 input = sys.stdin.readline
 
-def solution() :
-    t, w = map(int, input().split())
-    array = [0] + [int(input()) for _ in range(t)]
-    # 1. dp 생성
-    dp = [[0 for _ in range(w+1)] for _ in range(t+1)]
-    # 2.
-    for i in range(1, t+1) :
-        # 2-1. 움직이지 않는 경우
-        # 1번 나무의 경우
-        if array[i] == 1 : dp[i][0] = dp[i-1][0] + 1
-        # 2번 나무의 경우
-        else : dp[i][0] = dp[i-1][0]
-        # 2-2.
-        for j in range(1, w+1) :
-            # 현재 위치가 1번이고, 1번 나무에서 자두가 떨어질 경우 & 현재 위치가 2번이고, 2번 나무에서 자두가 떨어질 경우
-            if (j % 2 == 0 and array[i] == 1) or (j % 2 != 0 and array[i] == 2) : dp[i][j] = max(dp[i-1][j], dp[i-1][j-1]) + 1
-            # 자두를 먹지 못하는 경우
-            else : dp[i][j] = max(dp[i-1][j], dp[i-1][j-1])
-    # 3. 결과 출력
-    print(max(dp[-1][:]))
-    
-if __name__ == "__main__":
-    solution()
+T, W = map(int, input().split())
+
+# 1. dp 생성
+dp = [[0 for _ in range(W+1)] for _ in range(T+1)]
+# 2.
+for t in range(1, T+1):
+    # 2-1. 초기값 설정
+    dp[t][0] = dp[t-1][0] + int((tree:=int(input()))==1)
+    # 2-2.
+    for w in range(1, W+1 if t>=W else t+1):
+        # 2-2-1. 자두를 먹을 수 있는 경우
+        if (now:=w%2+1) == tree:
+            dp[t][w] = max(dp[t-1][w], dp[t-1][w-1]) + 1
+        # 2-2-2. 자두를 먹을 수 없는 경우
+        else :
+            dp[t][w] = dp[t-1][w]
+# 3. 결과 출력
+print(max(dp[-1]))
