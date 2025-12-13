02bb0a6bc229e8dd71044ebf6a5d45245f37dbfa
HyeonTae <47472389+Oneul-hyeon@users.noreply.github.com>
2025-12-14 01:33:52 +0900

    [Gold V] Title: 감시 피하기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/18428. 감시 피하기/감시 피하기.py
+++ 백준/Gold/18428. 감시 피하기/감시 피하기.py
@@ -1,51 +1,64 @@
 import sys
+from itertools import combinations
 input = sys.stdin.readline
 
-# 1. 감시 체크 함수 정의
-def check() :
+# 1. 체크 함수 정의
+def check():
     # 1-1.
-    for x, y in teacher :
+    for x, y in teacher:
         # 1-1-1.
-        for dir_x, dir_y in [(-1, 0), (0, 1), (1, 0), (0, -1)] :
-            for i in range(1, n) :
-                nx, ny = x + dir_x * i, y + dir_y * i
-                # 맵을 벗어나거나 장애물을 마주칠 경우 break
-                if (nx < 0 or nx >= n or ny < 0 or ny >= n) or graph[nx][ny] == "O" : break
-                # 학생이 있을 경우 False 반환
-                if graph[nx][ny] == "S" : return False
+        for dir_x, dir_y in [(-1, 0), (0, 1), (1, 0), (0, -1)]:
+            nx, ny = x, y
+            while True:
+                # 다음 위치 정의
+                nx, ny = nx+dir_x, ny+dir_y
+                # 예외 처리
+                if nx < 0 or nx >= N or ny < 0 or ny >= N or graph[nx][ny] == "O":
+                    break
+                # 다음 위치에 학생이 있는 경우 False 반환
+                if graph[nx][ny] == "S":
+                    return False
     # 1-2. True 반환
     return True
-# 2. 백트래킹 함수 정의
-def backtracking(cnt, idx) :
-    if idx == l : return
-    # 2-1. 장애물이 모두 설치된 경우
-    if cnt == 3 :
-        # 2-1-1. 감시를 모두 피할 경우 YES 출력 후 종료
-        if check() :
-            print("YES")
-            exit()
-        return
-    # 2-2.
-    for i in range(idx, l) :
-        x, y = empty[i]
-        # 2-2-1. 장애물 설치
+
+N = int(input())
+graph = [list(input().strip().split()) for _ in range(N)]
+
+# 2. 선생님 인덱스 리스트 초기화
+teacher = []
+# 3. 감시 가능 인덱스 집합 초기화
+potential = set()
+# 4.
+for x in range(N):
+    for y in range(N):
+        # 4-1. 해당 위치에 선생님이 있는 경우
+        if graph[x][y] == "T":
+            # 4-1-1. 선생님 인덱스 리스트 업데이트
+            teacher.append((x, y))
+            # 4-1-2.
+            for dir_x, dir_y in [(-1, 0), (0, 1), (1, 0), (0, -1)]:
+                nx, ny = x, y
+                while True:
+                    # 다음 위치 정의
+                    nx, ny = nx+dir_x, ny+dir_y
+                    # 예외 처리
+                    if nx < 0 or nx >= N or ny < 0 or ny >= N:
+                        break
+                    # 다음 위치가 빈 칸인 경우 감시 가능 인덱스 집합 업데이트
+                    if graph[nx][ny] == "X":
+                        potential.add((nx, ny))
+# 5.
+for combination in combinations(potential, 3):
+    # 5-1. 장애물 설치
+    for x, y in combination:
         graph[x][y] = "O"
-        # 2-2-2. 백트래킹 실행
-        backtracking(cnt + 1, i+1)
-        # 2-2-3. 장애물 회수
+    # 5-2. 모든 학생이 감시를 피할 수 있다면 YES 출력 후 break
+    if check():
+        print("YES")
+        break
+    # 5-3. 장애물 해제
+    for x, y in combination:
         graph[x][y] = "X"
-
-n = int(input())
-graph = [list(input().split()) for _ in range(n)]
-
-# 3. 선생님, 학생, 빈 곳 위치 리스트 생성
-teacher, empty = [], []
-for i in range(n) :
-    for j in range(n) :
-        if graph[i][j] == "T" : teacher.append((i, j))
-        elif graph[i][j] == "X" : empty.append((i, j))
-l = len(empty)
-# 4. 백트래킹 실행
-backtracking(0, 0)
-# 5. No 출력
-print("NO")
+# 6. NO 출력
+else:
+    print("NO")
