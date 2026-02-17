9b7f0f3c2802a5d335ddba982f20de49ddda612e
HyeonTae <47472389+Oneul-hyeon@users.noreply.github.com>
2026-02-17 13:55:55 +0900

    [Gold IV] Title: 알파벳, Time: 5476 ms, Memory: 160408 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.py
+++ 백준/Gold/1987. 알파벳/알파벳.py
@@ -1,39 +1,44 @@
 import sys
 input = sys.stdin.readline
 
-# 1. dfs 함수 정의
-def dfs(x, y, string) :
-    global max_len
-    # 1-1.
-    for dir_x, dir_y in dirs :
-        nx, ny = x + dir_x, y + dir_y
+# 1. 백트래킹 함수 정의
+def backtracking(x, y, cnt):
+    global ans, checked
 
-        # 예외처리
-        if nx < 0 or nx >= n or ny < 0 or ny >= m : continue
-        # 방문한 적이 있는 경우 최댓값 업데이트
-        if visited[nx][ny] : max_len = max(max_len, len(string))
-        # 방문한 적이 없는 경우
-        else :
-            # 해당 문자가 문자열에 이미 있는 경우 최댓값 업데이트
-            if board[nx][ny] in string : max_len = max(max_len, len(string))
-            # 해당 문자가 문자열에 없는 경우
-            else :
-                # 방문 처리
-                visited[nx][ny] = True
-                # dfs 실행
-                dfs(nx, ny, string + board[nx][ny])
-                # 방문 취소
-                visited[nx][ny] = False
-                
-n, m = map(int, input().split())
-board = [list(map(str, input().rstrip())) for _ in range(n)]
-dirs = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-string = board[0][0]
-max_len = -int(1e9)
+    # 1-1. 최대 칸 수 업데이트
+    ans = max(ans, cnt)
+    # 1-2.
+    for dir_x, dir_y in [(-1, 0), (0, 1), (1, 0), (0, -1)]:
+        # 1-2-1. 다음 위치 설정
+        nx, ny = x+dir_x, y+dir_y
+        # 1-2-2. 예외 처리
+        if nx<0 or nx>=R or ny<0 or ny>=C: continue
+        # 1-2-3. 다음 위치에 방문하지 않았으면서, 해당 알파벳을 사용하지 않는 경우
+        if not checked & (1 << (index:=ord(graph[nx][ny])-standard)) and not visited[nx][ny]:
+            # 방문 처리
+            visited[nx][ny] = True
+            # 알파벳 사용 처리
+            checked |= (1<<index)
+            # 백트래킹 실행
+            backtracking(nx, ny, cnt+1)
+            # 방문 해제
+            visited[nx][ny] = False
+            # 알파벳 사용 해제
+            checked &= ~(1<<index)
+
+R, C = map(int,input().split())
+graph = [list(input().strip()) for _ in range(R)]
+
 # 2. 방문 여부 리스트 생성
-visited = [[False] * m for _ in range(n)]
+visited = [[False for _ in range(C)] for _ in range(R)]
+# 3. 알파벳 사용 여부 변수 생성
+checked = 0
+# 4. 정답 변수 생성
+ans = 0
+# 5. 백트래킹 실행
+standard = ord("A")
 visited[0][0] = True
-# 3. dfs 실행
-dfs(0, 0, string)
-# 4. 결과 출력
-print(max_len)
+checked |= (1<<ord(graph[0][0])-standard)
+backtracking(0, 0, 1)
+# 6. 정답 출력
+print(ans)
